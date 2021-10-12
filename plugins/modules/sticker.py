ee44120875df2d63926b9ef3c17f23602f595e48
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-10-12 17:33:26 +0530

    Create sticker.py

---
+++ plugins/modules/sticker.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Copyright (C) @ZauteKm
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from pyrogram import filters
+from pyrogram import Client as MT_ID_Bot
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+from plugins.translation import Translation
+from plugins.config import Config
+
+UPDATE_CHANNEL=Config.UPDATE_CHANNEL # Update Channel Forces Subscribe
+BOT_USERNAME=Config.BOT_USERNAME # ReStart Option 
+JOIN=Translation.JOIN_TEXT # Button Text (Update Channel)
+TRY=Translation.TRY_TEXT # Button Text (Update Channel)
+SUB_TEXT=Translation.FSUB_TEXT # FSUB Information Text
+
+
+@ZauteKm.on_message(filters.private & ~filters.forwarded & ~filters.command(["start", "help", "about", "info", "information", "json", "source", "id"]))
+async def stickers(bot, msg):
+    update_channel = UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await bot.get_chat_member(update_channel, msg.chat.id)
+            if user.status == "kicked out":
+               await update.reply_text("😔 Sorry Dude, You are **BANNED 😜**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{Channel User Name} To Use Me") From Bot.py
+            await msg.reply_text(
+                text=f"<b>{SUB_TEXT}</b>",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text=f"{JOIN}", url=f"t.me/{UPDATE_CHANNEL}")],
+                    [ InlineKeyboardButton(text=f"{TRY}", url=f"https://t.me/{BOT_USERNAME}?start=try")]
+              ])
+            )
+            return
+        except Exception:
+            await msg.reply_text(f"@{UPDATE_CHANNEL}")
+            return  
+@ZauteKm.on_message(filters.command(["stickerid"]))
+async def stickerid(bot, message):   
+    if message.reply_to_message.sticker:
+       await message.reply(f"**Your Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
+    else: 
+       await message.reply("Hmmm, it's not a Sticker...!!!")
+    
