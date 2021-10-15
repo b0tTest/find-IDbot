21cf981155b10fcde1b38fd516e3ce9f07198db6
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-10-15 11:08:57 +0530

    Update buttons.py

--- plugins/commands/buttons.py
+++ plugins/commands/buttons.py
@@ -1,26 +1,26 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 START_BUTTON = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("👨‍💻 Developer", url=f"t.me/ZauteKm"),
-       InlineKeyboardButton("Open Source 💫", url=f"https://github.com/ZauteKm/tg_idsbot")
+       InlineKeyboardButton("👨‍💻 Creator", url=f"https://telegram.me/MyTestBotZ"),
+       InlineKeyboardButton("📝BotList", url=f"https://telegram.me/mybotzlist")
        ],[
-       InlineKeyboardButton("⬇️ More Information ⬇️", callback_data="help")
+       InlineKeyboardButton("🤔 How to Use meh", callback_data="help")
        ]]
        )
 
 HELP_BUTTON = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("🆔 Telegram ID", callback_data="id"),
+       InlineKeyboardButton("Telegram 🆔", callback_data="id"),
        InlineKeyboardButton("Telegram Info ℹ️", callback_data="info")
        ],[
        InlineKeyboardButton("🏠 Home", callback_data="start"),
-       InlineKeyboardButton("❌ Close", callback_data="close"),
-       InlineKeyboardButton("About 🧐", callback_data="about")
+       InlineKeyboardButton("💭 About", callback_data="about"),
+       InlineKeyboardButton("⛔ Close", callback_data="close")
        ]]
        )
 
 ABOUT_BUTTON = InlineKeyboardMarkup( [[
        InlineKeyboardButton("🔙 Back", callback_data="help"),
        InlineKeyboardButton("🏠 Home", callback_data="start"),
-       InlineKeyboardButton("Close ❌", callback_data="close")
+       InlineKeyboardButton("⛔ Close", callback_data="close")
        ]]
        )
