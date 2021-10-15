4eda829b6c02b73eb5471ec5eeaeddf8caca06a4
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-10-15 10:33:30 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -1,17 +1,3 @@
-#!/usr/bin/env python3
-# Copyright (C) @ZauteKm
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
 
@@ -26,7 +12,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
 
 
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "TGBotsProJect")
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "MyTestBotZ")
 
 
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "tg_idsbot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "findTgIDbot")
